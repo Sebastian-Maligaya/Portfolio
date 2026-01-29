dfc93b46f20c450f6126c0dfa87996f09fbe5788
Sebastian Maligaya <maligaya.286449@globalcity.sti.edu.ph>
2026-01-29 21:49:41 +0800

    Skills

--- Website-Portfolio/src/AboutMe/AboutMe.jsx
+++ Website-Portfolio/src/AboutMe/AboutMe.jsx
@@ -1,18 +1,65 @@
 import './AboutMe.css';
 import AboutMeImg from '../assets/AboutMe.png'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode } from "swiper/modules";
+import "swiper/css";
 
 function AboutMe() {
+
+    const logos = [
+        { name: "MySQL", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg" },
+        { name: "Python", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" },
+        { name: "Java", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" },
+        { name: "C#", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" },
+        { name: "JavaScript", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" },
+        { name: "React", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" },
+        { name: "CSS", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" },
+        { name: "NodeJS", src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" },
+    ];
+
     return (
-        <div className="about-me-container">
-            <div className="AboutMeLeft">
-                <h1>About Me</h1>
-                <p>Information Technology student with hands-on project experience in software  development,
-                    networking, and database management, I am eager to begin my professional  journey in the
-                    IT industry. I aim to contribute my technical expertise, problem-solving abilities,  and
-                    teamwork skills to an innovative company while continuing to grow as an IT professional.</p>
+        <div>
+            <div className="about-me-container">
+                <div className="AboutMeLeft">
+                    <h1>About Me</h1>
+                    <p>Information Technology student with hands-on project experience in software  development,
+                        networking, and database management, I am eager to begin my professional  journey in the
+                        IT industry. I aim to contribute my technical expertise, problem-solving abilities,  and
+                        teamwork skills to an innovative company while continuing to grow as an IT professional.</p>
+                </div>
+                <div className="AboutMeRight">
+                    <img src={AboutMeImg} alt="AboutMe" className="image" />
+                </div>
             </div>
-            <div className="AboutMeRight">
-                <img src={AboutMeImg} alt="AboutMe" className="image" />
+            <div className='Skills'>
+                <h1>Skills</h1>
+                <p>Programming Languages</p>
+
+                <div className="carousel">
+                    <Swiper
+                        modules={[Autoplay, FreeMode]}
+                        spaceBetween={1}
+                        slidesPerView={5}
+                        loop={true}
+                        grabCursor={true}
+                        autoplay={{
+                            delay: 1000,
+                            disableOnInteraction: false,
+                        }}
+                       
+                    >
+
+                        {logos.map((logo, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="Item-logo">
+                                    <img src={logo.src} alt={logo.name} />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+
+                    </Swiper>
+                </div>
+
             </div>
         </div>
     );
